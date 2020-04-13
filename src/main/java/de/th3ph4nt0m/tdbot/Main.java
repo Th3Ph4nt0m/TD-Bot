8b347c2ba917778bb3e492f6fdee04062ddcb41b
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-04-14 01:54:15 +0200

    - updated copyright

--- src/main/java/de/th3ph4nt0m/tdbot/Main.java
+++ src/main/java/de/th3ph4nt0m/tdbot/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 Henrik Steffens aka Th3Ph4nt0m
+ *
+ * $file.filename is part of the TD-Bot
+ * Last edit: 2020.4.14
+ */
+
 package de.th3ph4nt0m.tdbot;
 
 import net.dv8tion.jda.api.JDA;
--- src/main/java/de/th3ph4nt0m/tdbot/launcher/BotLauncher.java
+++ src/main/java/de/th3ph4nt0m/tdbot/launcher/BotLauncher.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 Henrik Steffens aka Th3Ph4nt0m
+ *
+ * $file.filename is part of the TD-Bot
+ * Last edit: 2020.4.14
+ */
+
 package de.th3ph4nt0m.tdbot.launcher;
 
 import de.th3ph4nt0m.tdbot.Main;
