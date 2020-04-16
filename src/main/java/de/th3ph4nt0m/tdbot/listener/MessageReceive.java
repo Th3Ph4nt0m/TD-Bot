fb5e3be3a8ca73d41f9c410145576d3e65e643cb
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-04-16 16:17:08 +0200

    - added main channel check

--- src/main/java/de/th3ph4nt0m/tdbot/listener/MessageReceive.java
+++ src/main/java/de/th3ph4nt0m/tdbot/listener/MessageReceive.java
@@ -1,17 +1,22 @@
 /*
  * Copyright (c) 2020 Henrik Steffens aka Th3Ph4nt0m
  *
- * $file.filename is part of the TD-Bot
+ * MessageReceive.java is part of the TD-Bot
  * Last edit: 2020.4.16
  */
 
 package de.th3ph4nt0m.tdbot.listener;
 
 import de.th3ph4nt0m.tdbot.Main;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public
@@ -21,6 +26,25 @@ class MessageReceive extends ListenerAdapter
     void onMessageReceived(MessageReceivedEvent event)
     {
         Message msg = event.getMessage();
+        if (event.getChannel().getId().equals("654374520248991764")) {
+            if (!event.getAuthor().getId().equals("699378436908777503")) {
+                msg.delete().queue();
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setColor(Color.RED).setTitle("Message deleted");
+                builder.setDescription("Your message was deleted because this channel is only for system messages!");
+                event.getChannel().sendMessage(builder.build()).queue();
+            } else {
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask()
+                {
+                    @Override public
+                    void run()
+                    {
+                        msg.delete().queue();
+                    }
+                }, 5000);
+            }
+        }
         if (msg.getContentRaw().startsWith("-") || msg.getAuthor().getId().equals("234395307759108106")) {
             {
                 MessageChannel channel = event.getChannel();
@@ -28,7 +52,11 @@ class MessageReceive extends ListenerAdapter
                     Message message = event.getMessage();
                     message.delete().queue();
                     MessageChannel groovy = Main.getInstance().getJda().getTextChannelById("656220001123958802");
-                    groovy.sendMessage(event.getAuthor().getAsMention() + " For groovy commands, please use this channel the next time!").queue();
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.setColor(Color.RED).setTitle("Wrong Channel").setDescription("For groovy commands, please use this channel the next time!");
+                    groovy.sendMessage(event.getAuthor().getAsMention()).queue();
+                    groovy.sendMessage(builder.build()).queue();
+//                        groovy.sendMessage(event.getAuthor().getAsMention() + " For groovy commands, please use this channel the next time!").queue();
                 }
             }
         }
