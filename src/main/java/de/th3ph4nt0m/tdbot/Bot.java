03d0b5423675c30a1dc7ae475b6aa8d0107afe39
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-06-02 20:14:31 +0200

    - enabled intent and cache for Presence

--- src/main/java/de/th3ph4nt0m/tdbot/Bot.java
+++ src/main/java/de/th3ph4nt0m/tdbot/Bot.java
@@ -2,13 +2,13 @@
  * Copyright (c) 2020 Henrik Steffens aka Th3Ph4nt0m
  *
  * Bot.java is part of the TD-Bot
- * Last edit: 2020.5.31
+ * Last edit: 2020.6.2
  */
 
 package de.th3ph4nt0m.tdbot;
 
+import de.th3ph4nt0m.tdbot.core.VoiceSystem;
 import de.th3ph4nt0m.tdbot.listener.*;
-import de.th3ph4nt0m.tdbot.utils.ChannelCreator;
 import de.th3ph4nt0m.tdbot.utils.MessageCenter;
 import de.th3ph4nt0m.tdbot.utils.MongoHandler;
 import net.dv8tion.jda.api.JDA;
@@ -16,9 +16,12 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.EventListener;
 import java.util.List;
 
@@ -29,16 +32,15 @@ class Bot implements EventListener
     private JDA jda;
     private static Bot instance;
     private final List<VoiceChannel> owChannels = new ArrayList<>();
-    private ChannelCreator channelCreator;
     private MongoHandler mongoHandler;
+    private VoiceSystem voiceSystem;
 
     public
     Bot()
     {
         instance = this;
         try {
-            this.jda = JDABuilder.createDefault("NzE2MDcwMjg3NzI4MzEyMzcy.XtGajA.wum_0T-ymhloL_31JzM7WdYfJXI").setAutoReconnect(true).setStatus(OnlineStatus.ONLINE).setActivity(Activity.watching("over TD-Nation")).build();
-            this.channelCreator = new ChannelCreator();
+            this.jda = JDABuilder.createDefault("NzE2MDcwMjg3NzI4MzEyMzcy.XtGajA.wum_0T-ymhloL_31JzM7WdYfJXI").setAutoReconnect(true).setStatus(OnlineStatus.ONLINE).setActivity(Activity.watching("over TD-Nation")).enableIntents(GatewayIntent.GUILD_PRESENCES).enableCache(EnumSet.of(CacheFlag.ACTIVITY)).build();
             this.mongoHandler = new MongoHandler();
             jda.addEventListener(new MessageReceive());
             jda.addEventListener(new VoiceConnect());
@@ -46,6 +48,7 @@ class Bot implements EventListener
             jda.addEventListener(new VoiceMove());
             jda.addEventListener(new CommandListener());
             jda.addEventListener(new ReactionListener());
+            this.voiceSystem = new VoiceSystem();
             jda.awaitReady();
 
             new MessageCenter(true);
@@ -71,14 +74,14 @@ class Bot implements EventListener
         return mongoHandler;
     }
 
+    public VoiceSystem getVoiceSystem()
+    {
+        return voiceSystem;
+    }
+
     public List<VoiceChannel> getOwChannels()
     {
         return owChannels;
-    }
-
-    public ChannelCreator getChannelCreator()
-    {
-        return channelCreator;
     }
 
 }
