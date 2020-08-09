0abb314df82e9f00a6557b1baadb22d9a0d84603
Th3Ph4nt0m <Development@th3ph4nt0m.de>
2020-08-09 21:33:00 +0200

    - removed Credentials interface

--- src/main/java/de/th3ph4nt0m/tdbot/interfaces/Credentials.java
+++
@@ -1,8 +0,0 @@
-package de.th3ph4nt0m.tdbot.interfaces;
-
-public interface Credentials {
-
-    //The credentials for the LostNameEU MongoDB Database are stored in this file. They are not pushed to github!!!
-
-    String test = "";
-}
