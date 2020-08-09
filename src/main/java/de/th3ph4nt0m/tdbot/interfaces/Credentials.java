bd133e3d9851e5fa535b7be692083cfe43fca215
Th3Ph4nt0m <Development@th3ph4nt0m.de>
2020-08-09 17:12:39 +0200

    - added Credentials.java

---
+++ src/main/java/de/th3ph4nt0m/tdbot/interfaces/Credentials.java
@@ -0,0 +1,6 @@
+package de.th3ph4nt0m.tdbot.interfaces;
+
+public interface Credentials {
+
+    //The credentials for the LostNameEU MongoDB Database are stored in this file. They are not pushed to github!!!
+}
