1d610a2969509c079b3187dd71e4c2d1787d1567
Dominik Lenz <dominikalexanderlenz@gmail.com>
2020-08-23 20:18:15 +0200

    code cleanup (revert if errors occur)

--- src/main/java/de/th3ph4nt0m/tdbot/core/CommandHandler.java
+++ src/main/java/de/th3ph4nt0m/tdbot/core/CommandHandler.java
@@ -17,7 +17,7 @@ public
 class CommandHandler
 {
 
-    public static final CommandParser parse = new CommandParser();
+    //public static final CommandParser parse = new CommandParser();
     public static HashMap <String, ICommand> commands = new HashMap <>();
 
     public static
@@ -28,10 +28,8 @@ class CommandHandler
 
             if (!safe) {
                 commands.get(cmd.invoke).action(cmd.args, cmd.event);
-                commands.get(cmd.invoke).excecuted(safe, cmd.event);
-            } else {
-                commands.get(cmd.invoke).excecuted(safe, cmd.event);
             }
+            commands.get(cmd.invoke).excecuted(safe, cmd.event);
         }
     }
 }
