47d8a369f30d91655572c83452cebeefa5f6ee5b
Dominik <dominikalexanderlenz@gmail.com>
2020-09-12 14:46:49 +0200

    -fixed potential errors

--- src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
+++ src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
@@ -16,6 +16,8 @@ import de.th3ph4nt0m.tdbot.utils.MessageCenter;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.util.Objects;
+
 public class CMD_userinfo implements ICommand {
     @Override
     public boolean called(String[] args, MessageReceivedEvent event) {
@@ -24,10 +26,10 @@ public class CMD_userinfo implements ICommand {
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
-        String[] contentRaw = event.getMessage().getContentRaw().split(" ");
+       // String[] contentRaw = event.getMessage().getContentRaw().split(" ");
         Member m = event.getMessage().getMentionedMembers().get(0);
         NationMember nationMember = new NationMember(m, m.getId());
-        if (event.getMember().getRoles().contains(Bot.getInstance().getJda().getRoleById(Bot.getInstance().getProperty().get("bot", "bot.highestRole"))) && event.getChannel().getId().equals(Bot.getInstance().getProperty().get("bot", "bot.adminChannelID"))) {
+        if (Objects.requireNonNull(event.getMember()).getRoles().contains(Bot.getInstance().getJda().getRoleById(Bot.getInstance().getProperty().get("bot", "bot.highestRole"))) && event.getChannel().getId().equals(Bot.getInstance().getProperty().get("bot", "bot.adminChannelID"))) {
             event.getChannel().sendMessage(nationMember.getInfo()).queue();
         } else {
             MessageCenter.getInstance().sendNoAccess(event.getChannel().getId());
