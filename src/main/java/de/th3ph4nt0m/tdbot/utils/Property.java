c930aa6ad9d6122f5e6471d8cfce7c5b6fea29c7
Dominik <dominikalexanderlenz@gmail.com>
2020-10-31 20:47:51 +0100

    -removed unused property

--- src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
+++ src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
@@ -70,7 +70,6 @@ public class Property
                 prop.setProperty("bot.autoprint", "0");
                 prop.setProperty("bot.rulesID", "RuleChannelID");
                 prop.setProperty("bot.groovyChannelID","GroovyChannelID");
-                prop.setProperty("bot.groovyID","GroovyID");
                 prop.setProperty("bot.afkID","AFKChannelID");
                 prop.setProperty("bot.createID","CreateChannelID");
                 prop.setProperty("bot.compID","CompCreateChannelID");
