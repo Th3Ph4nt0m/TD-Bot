2e83e64d16a16b4aa2ed0f8a70c4880b330085ae
Dominik <dominikalexanderlenz@gmail.com>
2020-10-31 21:59:17 +0100

    -alternative auto recognition of admin role

--- src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
+++ src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
@@ -28,6 +28,7 @@ public class CMD_userinfo implements ICommand {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         Role admin = event.getGuild().getRoles().stream().filter(role-> role.getPosition() == 1).findAny().orElse(null);
+        // Role admin = event.getGuild().getRolesByName("OP",false) ;
         Member m = event.getMessage().getMentionedMembers().get(0);
         NationMember nationMember = new NationMember(m, m.getId());
         if (event.getMember().getRoles().contains(admin) && event.getChannel().getId().equals(Bot.getInstance().getProperty().get("bot", "bot.adminChannelID"))) {
