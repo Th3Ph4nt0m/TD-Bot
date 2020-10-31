84028a5e73bacd04040e49b45b72c142a2bd553e
Dominik <dominikalexanderlenz@gmail.com>
2020-10-31 21:11:46 +0100

    -added documentation and safer reaction system

--- src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
+++ src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
@@ -69,6 +69,7 @@ public class Property
                 prop.setProperty("bot.token", "token");
                 prop.setProperty("bot.autoprint", "0");
                 prop.setProperty("bot.rulesID", "RuleChannelID");
+                prop.setProperty("bot.ruleReactionID", "RuleReactionID");
                 prop.setProperty("bot.groovyChannelID","GroovyChannelID");
                 prop.setProperty("bot.afkID","AFKChannelID");
                 prop.setProperty("bot.createID","CreateChannelID");
