893c5fe867b865fd2414b8d10ea5a308a56c9c38
Dominik <dominikalexanderlenz@gmail.com>
2020-10-31 21:54:50 +0100

    -auto recognizing admin role now

--- src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
+++ src/main/java/de/th3ph4nt0m/tdbot/utils/Property.java
@@ -69,11 +69,11 @@ public class Property
                 prop.setProperty("bot.token", "token");
                 prop.setProperty("bot.autoprint", "0");
                 prop.setProperty("bot.rulesID", "RuleChannelID");
+                prop.setProperty("bot.ruleReactionID", "RuleReactionID");
                 prop.setProperty("bot.groovyChannelID","GroovyChannelID");
                 prop.setProperty("bot.afkID","AFKChannelID");
                 prop.setProperty("bot.createID","CreateChannelID");
                 prop.setProperty("bot.compID","CompCreateChannelID");
-                prop.setProperty("bot.highestRole","HighestRoleID");
                 prop.setProperty("bot.adminChannelID","AdminChannelID");
 
                 // save properties to project folder
