364737f6ac4a59d37f04282665928776cc46275f
Dominik <dominikalexanderlenz@gmail.com>
2020-12-29 15:29:27 +0100

    -updated last edits and version number

--- src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
+++ src/main/java/de/th3ph4nt0m/tdbot/commands/CMD_userinfo.java
@@ -17,7 +17,7 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
- Last edit: 2020/11/2
+ Last edit: 2020/12/29
  ******************************************************************************/
 
 package de.th3ph4nt0m.tdbot.commands;
